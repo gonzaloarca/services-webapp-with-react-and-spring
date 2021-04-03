54c45ef2cbb4d29480228268f0cdaedc48a99726
Manuel Rodriguez <manurodriguez@itba.edu.ar>
2021-04-03 18:03:03 -0300

    FIX: Tests de JobPost empezados

--- persistence/src/main/java/ar/edu/itba/paw/persistence/jdbc/JobPostDaoJDBC.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/jdbc/JobPostDaoJDBC.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.persistence.jdbc;
 
 import ar.edu.itba.paw.interfaces.dao.JobPostDao;
+import ar.edu.itba.paw.interfaces.dao.UserDao;
 import ar.edu.itba.paw.models.JobPost;
 import ar.edu.itba.paw.models.JobPost.Zone;
 import ar.edu.itba.paw.models.User;
@@ -15,114 +16,113 @@ import java.util.*;
 
 @Repository
 public class JobPostDaoJDBC implements JobPostDao {
-    @Override
-    public JobPost create(long userId, String title, String availableHours, JobPost.JobType jobType, List<Zone> zones) {
-        return null;
-    }
 
-    @Override
-    public Optional<JobPost> findById(long id) {
-        return Optional.empty();
-    }
+    @Autowired
+    private UserDao userDao;
 
-    @Override
-    public Optional<List<JobPost>> findByUserId(long id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<List<JobPost>> findByJobType(JobPost.JobType jobType) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<List<JobPost>> findByZone(Zone zone) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<List<JobPost>> findAll() {
-        return Optional.empty();
-    }
-
-    private static List<Zone> auxiGetZones(Object[] objs){
+    private static List<Zone> auxiGetZones(Object[] objs) {
         List<Zone> zones = new ArrayList<>();
         Arrays.stream(objs)
                 .forEach(obj -> zones.add(Zone.values()[(int) obj]));
         return zones;
     }
-//
-//    private final static RowMapper<JobPost> JOB_POST_ROW_MAPPER = (resultSet, rowNum) -> new JobPost(
-//            resultSet.getLong("id"),
-//            resultSet.getLong("user_id"),
-//            resultSet.getString("title"),
-//            resultSet.getString("available_hours"),
-//            JobPost.JobType.values()[resultSet.getInt("job_type")],
-//            JobPostDaoJDBC.auxiGetZones((Object[]) resultSet.getArray("zones").getArray()),
-//            resultSet.getBoolean("is_active"));
-//
-//    private final JdbcTemplate jdbcTemplate;
-//    private final SimpleJdbcInsert jdbcInsert;
-//    private final SimpleJdbcInsert jdbcInsertZone;
-//
-//    @Autowired
-//    public JobPostDaoJDBC(DataSource ds) {
-//        jdbcTemplate = new JdbcTemplate(ds);
-//        jdbcInsert = new SimpleJdbcInsert(ds).withTableName("job_post").usingGeneratedKeyColumns("id");
-//        jdbcInsertZone = new SimpleJdbcInsert(ds).withTableName("post_zone");
-//    }
-//
-//    @Override
-//    public JobPost create(User userId, String title, String availableHours, JobPost.JobType jobType, List<Zone> zones) {
-//        Number key = jdbcInsert.executeAndReturnKey(new HashMap<String,Object>(){{
-//            put("user_id",userId);
-//            put("title",title);
-//            put("available_hours",availableHours);
-//            put("job_type",jobType.ordinal());
-//            put("is_active",true);
-//        }});
-//
-//        for (Zone zone : zones) {
-//            jdbcInsertZone.execute(new HashMap<String,Integer>(){{
-//                put("post_id",key.intValue());
-//                put("zone_id",zone.ordinal());
-//            }});
-//        }
-//
-//        JobPost jobPost = new JobPost();
-//        jobPost.setId(key.longValue());
-//        jobPost.setUser(userId);
-//        jobPost.setTitle(title);
-//        jobPost.setAvailableHours(availableHours);
-//        jobPost.setJobType(jobType);
-//        jobPost.setZones(zones);
-//        jobPost.setActive(true);
-//        return jobPost;
-//    }
-//
-//    @Override
-//    public Optional<JobPost> findById(long id) {
-//        return jdbcTemplate.query("SELECT id,user_id,title,available_hours,job_type,array_agg(pz.zone_id) as zones,is_active FROM job_post INNER JOIN post_zone pz on job_post.id = pz.post_id WHERE id = ? GROUP BY job_post.id",new Object[]{id},JOB_POST_ROW_MAPPER).stream().findFirst();
-//
-//    }
-//    @Override
-//    public Optional<List<JobPost>> findByUserId(long id) {
-//        return Optional.of(jdbcTemplate.query("SELECT * FROM job_post INNER JOIN post_zone pz on job_post.id = pz.post_id WHERE user_id = ? GROUP BY job_post.id",new Object[]{id},JOB_POST_ROW_MAPPER));
-//    }
-//
-//    @Override
-//    public Optional<List<JobPost>> findByJobType(JobPost.JobType jobType) {
-//        return Optional.of(jdbcTemplate.query("SELECT * FROM job_post INNER JOIN post_zone pz on job_post.id = pz.post_id WHERE job_type = ? GROUP BY job_post.id",new Object[]{jobType},JOB_POST_ROW_MAPPER));
-//    }
-//
-//    @Override
-//    public Optional<List<JobPost>> findByZone(JobPost.Zone zone) {
-//        return Optional.of(jdbcTemplate.query("SELECT * FROM job_post INNER JOIN post_zone pz on job_post.id = pz.post_id AND pz.zone_id  = ?",
-//                new Object[]{zone},JOB_POST_ROW_MAPPER));
-//    }
-//
-//    @Override
-//    public Optional<List<JobPost>> findAll() {
-//        return Optional.of(jdbcTemplate.query("SELECT * FROM job_post INNER JOIN post_zone pz on job_post.id = pz.post_id GROUP BY job_post.id",JOB_POST_ROW_MAPPER));
-//    }
+
+    private final static RowMapper<JobPost> JOB_POST_ROW_MAPPER = (resultSet, rowNum) -> new JobPost(
+            resultSet.getLong("id"),
+            new User(
+                    resultSet.getLong("user_id"),
+                    resultSet.getString("user_email"),
+                    resultSet.getString("user_username"),
+                    "",
+                    resultSet.getString("user_phone"),
+                    resultSet.getBoolean("user_is_professional"),
+                    resultSet.getBoolean("user_is_active")
+            ),
+            resultSet.getString("title"),
+            resultSet.getString("available_hours"),
+            JobPost.JobType.values()[resultSet.getInt("job_type")],
+            JobPostDaoJDBC.auxiGetZones((Object[]) resultSet.getArray("zones").getArray()),
+            resultSet.getBoolean("is_active"));
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+    private final SimpleJdbcInsert jdbcInsertZone;
+
+    @Autowired
+    public JobPostDaoJDBC(DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+        jdbcInsert = new SimpleJdbcInsert(ds).withTableName("job_post").usingGeneratedKeyColumns("id");
+        jdbcInsertZone = new SimpleJdbcInsert(ds).withTableName("post_zone");
+    }
+
+    @Override
+    public JobPost create(long userId, String title, String availableHours, JobPost.JobType jobType, List<Zone> zones) {
+        Number key = jdbcInsert.executeAndReturnKey(new HashMap<String, Object>() {{
+            put("user_id", userId);
+            put("title", title);
+            put("available_hours", availableHours);
+            put("job_type", jobType.ordinal());
+            put("is_active", true);
+        }});
+
+        //TODO: Cambiar excepciones por excepciones propias
+        User user = userDao.findById(userId).orElseThrow(NoSuchElementException::new);
+
+        for (Zone zone : zones) {
+            jdbcInsertZone.execute(new HashMap<String, Integer>() {{
+                put("post_id", key.intValue());
+                put("zone_id", zone.ordinal());
+            }});
+        }
+
+        return new JobPost(key.longValue(), user, title, availableHours, jobType, zones, true);
+    }
+
+    @Override
+    public Optional<JobPost> findById(long id) {
+        return jdbcTemplate.query(
+                "SELECT id,user_id,title,available_hours,job_type,array_agg(pz.zone_id) as zones,is_active FROM job_post " +
+                        "NATURAL JOIN (SELECT id AS user_id, username, email, phone, is_professional, is_active FROM users) AS users" +
+                        "INNER JOIN post_zone pz on id = pz.post_id WHERE id = ? " +
+                        "GROUP BY id",
+                new Object[]{id}, JOB_POST_ROW_MAPPER).stream().findFirst();
+    }
+
+    @Override
+    public Optional<List<JobPost>> findByUserId(long id) {
+        return Optional.of(jdbcTemplate.query(
+                "SELECT id,user_id,title,available_hours,job_type,array_agg(pz.zone_id) as zones,is_active FROM job_post " +
+                        "NATURAL JOIN (SELECT id AS user_id, username, email, phone, is_professional, is_active FROM users) AS users" +
+                        "INNER JOIN post_zone pz on id = pz.post_id WHERE user_id = ? " +
+                        "GROUP BY id",
+                new Object[]{id}, JOB_POST_ROW_MAPPER));
+    }
+
+    @Override
+    public Optional<List<JobPost>> findByJobType(JobPost.JobType jobType) {
+        return Optional.of(jdbcTemplate.query(
+                "SELECT id,user_id,title,available_hours,job_type,array_agg(pz.zone_id) as zones,is_active FROM job_post " +
+                        "NATURAL JOIN (SELECT id AS user_id, username, email, phone, is_professional, is_active FROM users) AS users" +
+                        "INNER JOIN post_zone pz on id = pz.post_id WHERE job_type = ? " +
+                        "GROUP BY id",
+                new Object[]{jobType}, JOB_POST_ROW_MAPPER));
+    }
+
+    @Override
+    public Optional<List<JobPost>> findByZone(JobPost.Zone zone) {
+        return Optional.of(jdbcTemplate.query(
+                "SELECT id,user_id,title,available_hours,job_type,array_agg(pz.zone_id) as zones,is_active FROM job_post " +
+                        "NATURAL JOIN (SELECT id AS user_id, username, email, phone, is_professional, is_active FROM users) AS users" +
+                        "INNER JOIN post_zone pz on id = pz.post_id AND pz.zone_id = ? ",
+                new Object[]{zone}, JOB_POST_ROW_MAPPER));
+    }
+
+    @Override
+    public Optional<List<JobPost>> findAll() {
+        return Optional.of(jdbcTemplate.query(
+                "SELECT id,user_id,title,available_hours,job_type,array_agg(pz.zone_id) as zones,is_active FROM job_post " +
+                        "NATURAL JOIN (SELECT id AS user_id, username, email, phone, is_professional, is_active FROM users) AS users" +
+                        "INNER JOIN post_zone pz on id = pz.post_id GROUP BY id",
+                JOB_POST_ROW_MAPPER));
+    }
 }
