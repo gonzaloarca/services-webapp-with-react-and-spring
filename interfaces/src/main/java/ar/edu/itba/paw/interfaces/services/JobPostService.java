76ba3e608406d0139c3eeabbdc98765e808b0491
Francisco Quesada <fquesada@itba.edu.ar>
2021-04-04 15:08:30 -0300

    FEAT: UserService yJobPost Service imlpementados

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/JobPostService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/JobPostService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface JobPostService {
 
-    JobPost create(long userId, String title, String availableHours, JobPost.JobType jobType, List<JobPost.Zone> zones);
+    JobPost create(String email, String username, String phone, String title, String availableHours, JobPost.JobType jobType, List<JobPost.Zone> zones);
 
     Optional<JobPost> findById(long id);
 
