2c237da0de704c101adfa1aabdbef6bd1277c055
Manuel Rodriguez <manurodriguez@itba.edu.ar>
2021-04-13 22:32:51 -0300

    FIX: exceptionController class name

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ExceptionController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ExceptionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class ExceptionController {
 
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     @org.springframework.web.bind.annotation.ExceptionHandler(
