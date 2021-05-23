f8d28634caff06d0e1cee2ffd8ec62b2905d566c
Gonzalo Arca <goarca@itba.edu.ar>
2021-05-23 17:57:15 -0300

    CHORE: Test de VerificationToken empezado

---
+++ persistence/src/test/java/jdbc/VerificationTokenDaoTest.java
@@ -0,0 +1,52 @@
+package jdbc;
+
+import config.TestConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+//TODO: renombrar a JPA
+@Rollback
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Sql("classpath:db_data_test.sql")
+@Transactional
+public class VerificationTokenDaoTest {
+    // TODO: Implementar tests
+
+    @Test
+    public void testFindByInvalidUserId() {
+
+    }
+
+    @Test
+    public void testFindByUnverifiedUserId() {
+
+    }
+
+    @Test
+    public void testFindByVerifiedUserId() {
+
+    }
+
+    @Test
+    public void testCreate() {
+
+    }
+
+    @Test
+    public void testDeleteTokenWithValidUser() {
+
+    }
+
+    @Test
+    public void testDeleteTokenWithInvalidUser() {
+
+    }
+
+
+}
