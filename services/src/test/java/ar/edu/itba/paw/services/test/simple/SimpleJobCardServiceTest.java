e597b226d0f2ec761471bfed0531db8140383cea
Francisco Quesada <fquesada@itba.edu.ar>
2021-06-10 15:18:58 -0300

    FIX: arreglado simple job card service test

--- services/src/test/java/ar/edu/itba/paw/services/test/simple/SimpleJobCardServiceTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/test/simple/SimpleJobCardServiceTest.java
@@ -4,14 +4,19 @@ import ar.edu.itba.paw.interfaces.HirenetUtils;
 import ar.edu.itba.paw.interfaces.dao.JobCardDao;
 import ar.edu.itba.paw.models.JobPost;
 import ar.edu.itba.paw.services.simple.SimpleJobCardService;
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,6 +30,12 @@ public class SimpleJobCardServiceTest {
 
     @Mock
     private JobCardDao jobCardDao;
+
+    @Mock
+    private MessageSource messageSource;
+
+    @Mock
+    private LevenshteinDistance levenshteinDistance;
 
     @Test
     public void testSearchWithoutCategory() {
@@ -46,5 +57,20 @@ public class SimpleJobCardServiceTest {
 
         Mockito.verify(jobCardDao).searchWithCategory(Mockito.eq(QUERY), Mockito.eq(ZONE),
                 Mockito.eq(JobPost.JobType.BABYSITTING), Mockito.eq(new ArrayList<>()), Mockito.eq(null), Mockito.eq(HirenetUtils.ALL_PAGES));
+    }
+
+    @Test
+    public void testGetSimilarTypesTest(){
+        String similarType = "Plombero";
+        String typeName = "Plomero";
+        //Calculado con herramienta externa
+        int levenshtein = 1;
+        Mockito.when(messageSource.getMessage(Mockito.anyString(),Mockito.isNull(),Mockito.eq(Locale.getDefault()))).thenReturn(typeName);
+        Mockito.when(levenshteinDistance.apply(Mockito.eq(similarType.toLowerCase()),Mockito.eq(typeName))).thenReturn(levenshtein);
+
+        List<JobPost.JobType> similarTypes = simpleJobCardService.getSimilarTypes(similarType, Locale.getDefault());
+
+        Assert.assertFalse(similarTypes.isEmpty());
+        Assert.assertTrue(similarTypes.contains(JobPost.JobType.PLUMBING));
     }
 }
