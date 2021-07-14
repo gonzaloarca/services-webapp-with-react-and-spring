017bbda9f9d67e74e13b1ed0894b471a81322c76
mparmaITBA <mparma@itba.edu.ar>
2021-07-14 15:35:29 -0300

    FIX: i18next sets language from browser

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -25,6 +25,9 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    detection: {
+      order: ['navigator']
+    },
     fallbackLng: 'es',
     react: {
       useSuspense: false,
