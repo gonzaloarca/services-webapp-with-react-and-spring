69da122a95e9f39b8f2b0402e74d8f33704a7b18
Manuel Rodriguez <manurodriguez@itba.edu.ar>
2021-07-15 18:39:54 -0300

    CHORE: Validacion en cada step de register

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -26,17 +26,87 @@ const useStyles = makeStyles(LoginAndRegisterStyles);
 const Register = () => {
   const classes = useStyles();
   const { t } = useTranslation();
-  const [values, setValues] = React.useState({
-    currentStep: 0,
-  });
 
-  const initialValues = {
+  const [data, setData] = React.useState({
     username: '',
     phone: '',
     email: '',
     password: '',
     passwordrepeat: '',
     image: '',
+  });
+
+  const handleNextStep = (newData, final = false) => {
+    if (final) {
+      makeRequest(newData);
+      return;
+    }
+    setData((prev) => ({ ...prev, ...newData }));
+    setCurrentStep((prev) => prev + 1);
+  };
+
+  const handlePrevStep = (newData) => {
+    setData((prev) => ({ ...prev, ...newData }));
+    setCurrentStep((prev) => prev - 1);
+  };
+
+  const [currentStep, setCurrentStep] = React.useState(0);
+  const steps = [
+    <StepOne next={handleNextStep} data={data} />,
+    <StepTwo next={handleNextStep} prev={handlePrevStep} data={data} />,
+  ];
+
+  const makeRequest = (newData) => {
+    console.log(newData);
+    //TODO: IMPLEMENTAR
+  };
+
+  return (
+    <div>
+      <NavBar currentSection={'/register'} isTransparent />
+      <div
+        className={classes.background}
+        style={{ backgroundImage: `url(./img/background.jpg)` }}
+      >
+        <div className={classes.cardContainer}>
+          <div className={classes.titleContainer}>
+            <img
+              src="/img/adduser.svg"
+              alt={t('register.title')}
+              loading="lazy"
+            />
+            <p className={classes.title}>{t('register.into')}</p>
+          </div>
+          <Card className={clsx(classes.customCard, classes.registerCard)}>
+            {steps[currentStep]}
+
+            {/* <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={onSubmit}
+            ></Formik> */}
+            <span className={classes.bottomLabel}>
+              <p>{t('register.alreadyhasaccount')}</p>
+              <Link
+                component={RouterLink}
+                to="/login"
+                className={classes.bottomLabelLink}
+              >
+                {t('register.login')}
+              </Link>
+            </span>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const StepOne = (props) => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+  const handleSubmit = (values) => {
+    props.next(values);
   };
 
   const validationSchema = Yup.object({
@@ -59,214 +129,191 @@ const Register = () => {
       .max(100, t('validationerror.maxlength', { length: 100 }))
       .min(8, t('validationerror.minlength', { length: 8 }))
       .oneOf([Yup.ref('password'), null], t('validationerror.passwordrepeat')),
+  });
+  console.log(props.data);
+  return (
+    <Formik
+      initialValues={props.data}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ values }) => (
+        <Form>
+          <Grid container spacing={3}>
+            <Grid item sm={7} xs={12}>
+              <Field
+                as={FormControl}
+                name="username"
+                required
+                fullWidth
+                variant="filled"
+                className={classes.FieldHeight}
+              >
+                <InputLabel>{t('register.username')}</InputLabel>
+                <FilledInput id="username" required />
+                <FormHelperText>
+                  <ErrorMessage name="username" />
+                </FormHelperText>
+              </Field>
+            </Grid>
+            <Grid item sm={5} xs={12}>
+              <Field
+                as={FormControl}
+                name="phone"
+                required
+                fullWidth
+                variant="filled"
+                className={classes.FieldHeight}
+              >
+                <InputLabel>{t('register.phone')}</InputLabel>
+                <FilledInput id="phone" required />
+                <FormHelperText>
+                  <ErrorMessage name="phone" />
+                </FormHelperText>
+              </Field>
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                as={FormControl}
+                name="email"
+                required
+                fullWidth
+                variant="filled"
+                className={classes.FieldHeight}
+              >
+                <InputLabel>{t('register.email')}</InputLabel>
+                <FilledInput id="email" type="email" required />
+                <FormHelperText>
+                  <ErrorMessage name="email" />
+                </FormHelperText>
+              </Field>
+            </Grid>
+            <Grid item sm={6} xs={12}>
+              <FormControlPassword
+                required
+                placeholder={t('register.password')}
+                variable="password"
+                fullWidth
+              />
+            </Grid>
+            <Grid item sm={6} xs={12}>
+              <FormControlPassword
+                required
+                placeholder={t('register.passwordrepeat')}
+                variable="passwordrepeat"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                className={clsx(classes.submitButton, 'mb-4')}
+                type="submit"
+              >
+                {t('register.continue')}
+              </Button>
+            </Grid>
+          </Grid>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+const StepTwo = (props) => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+  const handleSubmit = (values) => {
+    props.next(values, true);
+  };
+
+  const validationSchema = Yup.object({
     image: Yup.mixed()
       .test(
         'is-correct-type',
         t('validationerror.avatarfile.type'),
         (file) =>
-          file === '' ||
-          (file && ['image/png', 'image/jpeg'].includes(file.type))
+          file === undefined || ['image/png', 'image/jpeg'].includes(file.type)
       )
       .test(
         'is-correct-size',
         t('validationerror.avatarfile.size', { size: 2 }),
-        (file) => file === '' || (file && file.size <= 2 * 1024 * 1024)
+        (file) => file === undefined || file.size <= 2 * 1024 * 1024
       ),
   });
 
-  const onSubmit = (values, props) => {
-    console.log(values); //TODO: REGISTRAR
-  };
-
-  const changeToStep = (newStep) => {
-    setValues({ ...values, currentStep: newStep });
-  };
-
   return (
-    <div>
-      <NavBar currentSection={'/register'} isTransparent />
-      <div
-        className={classes.background}
-        style={{ backgroundImage: `url(./img/background.jpg)` }}
-      >
-        <div className={classes.cardContainer}>
-          <div className={classes.titleContainer}>
-            <img
-              src="/img/adduser.svg"
-              alt={t('register.title')}
-              loading="lazy"
-            />
-            <p className={classes.title}>{t('register.into')}</p>
-          </div>
-          <Card className={clsx(classes.customCard, classes.registerCard)}>
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={onSubmit}
-            >
-              {(props) => (
-                <Form>
-                  {values.currentStep <= 0 ? (
-                    <Grid container spacing={3}>
-                      <Grid item sm={7} xs={12}>
-                        <Field
-                          as={FormControl}
-                          name="username"
-                          required
-                          fullWidth
-                          variant="filled"
-                          className={classes.FieldHeight}
-                        >
-                          <InputLabel>{t('register.username')}</InputLabel>
-                          <FilledInput id="username" required />
-                          <FormHelperText>
-                            <ErrorMessage name="username" />
-                          </FormHelperText>
-                        </Field>
-                      </Grid>
-                      <Grid item sm={5} xs={12}>
-                        <Field
-                          as={FormControl}
-                          name="phone"
-                          required
-                          fullWidth
-                          variant="filled"
-                          className={classes.FieldHeight}
-                        >
-                          <InputLabel>{t('register.phone')}</InputLabel>
-                          <FilledInput id="phone" required />
-                          <FormHelperText>
-                            <ErrorMessage name="phone" />
-                          </FormHelperText>
-                        </Field>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Field
-                          as={FormControl}
-                          name="email"
-                          required
-                          fullWidth
-                          variant="filled"
-                          className={classes.FieldHeight}
-                        >
-                          <InputLabel>{t('register.email')}</InputLabel>
-                          <FilledInput id="email" type="email" required />
-                          <FormHelperText>
-                            <ErrorMessage name="email" />
-                          </FormHelperText>
-                        </Field>
-                      </Grid>
-                      <Grid item sm={6} xs={12}>
-                        <FormControlPassword
-                          required
-                          placeholder={t('register.password')}
-                          variable="password"
-                          fullWidth
-                        />
-                      </Grid>
-                      <Grid item sm={6} xs={12}>
-                        <FormControlPassword
-                          required
-                          placeholder={t('register.passwordrepeat')}
-                          variable="passwordrepeat"
-                          fullWidth
-                        />
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Button
-                          fullWidth
-                          className={clsx(classes.submitButton, 'mb-4')}
-                          onClick={() => changeToStep(1)}
-                        >
-                          {t('register.continue')}
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  ) : (
-                    <div>
-                      <p className={clsx(classes.subtitle, 'mb-3')}>
-                        {t('register.selectimage')}
-                      </p>
+    <Formik
+      initialValues={props.data}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ values, setFieldValue }) => (
+        <Form>
+          <div>
+            <p className={clsx(classes.subtitle, 'mb-3')}>
+              {t('register.selectimage')}
+            </p>
 
-                      <div className={clsx('flex justify-center mb-3')}>
-                        {console.log(props.values.image)}
-                        <img
-                          className={'rounded-full h-48 w-48 object-cover'}
-                          id="img-preview"
-                          src={
-                            props.values.image === ''
-                              ? './img/defaultavatar.svg'
-                              : URL.createObjectURL(props.values.image)
-                          }
-                          alt=""
-                        />
-                      </div>
-                      <p className={'mb-3 text-center'}>
-                        {t('register.imagepreview')}
-                      </p>
-                      <Grid container className={'mb-3 justify-center'}>
-                        <Grid item>
-                          <Input
-                            name="image"
-                            onChange={(event) =>
-                              event.target.files[0] !== undefined &&
-                              props.setFieldValue(
-                                'image',
-                                event.target.files[0]
-                              )
-                            }
-                            type="file"
-                          />
-                          <GreyButton
-                            disabled={props.values.image === ''}
-                            onClick={() => props.setFieldValue('image', '')}
-                          >
-                            {t('register.discardimage')}
-                          </GreyButton>
-                          <FormHelperText>
-                            <ErrorMessage name="image" />
-                          </FormHelperText>
-                        </Grid>
-                      </Grid>
-                      <p className={'mb-5 text-gray-500'}>
-                        {t('register.filedisclaimer', { size: 2 })}
-                      </p>
-                      <div className="flex justify-center">
-                        <GreyButton
-                          className={'mb-4 align-center'}
-                          onClick={() => changeToStep(0)}
-                        >
-                          {t('register.goback')}
-                        </GreyButton>
-                      </div>
-                      <div className="flex justify-center">
-                        <Button
-                          fullWidth
-                          className={clsx(classes.submitButton, 'mb-4')}
-                          type="submit"
-                        >
-                          {t('register.submit')}
-                        </Button>
-                      </div>
-                    </div>
-                  )}
-                </Form>
-              )}
-            </Formik>
-            <span className={classes.bottomLabel}>
-              <p>{t('register.alreadyhasaccount')}</p>
-              <Link
-                component={RouterLink}
-                to="/login"
-                className={classes.bottomLabelLink}
+            <div className={clsx('flex justify-center mb-3')}>
+              {console.log(values.image)}
+              <img
+                className={'rounded-full h-48 w-48 object-cover'}
+                id="img-preview"
+                src={
+                  values.image === ''
+                    ? './img/defaultavatar.svg'
+                    : URL.createObjectURL(values.image)
+                }
+                alt=""
+              />
+            </div>
+            <p className={'mb-3 text-center'}>{t('register.imagepreview')}</p>
+            <Grid container className={'mb-3 justify-center'}>
+              <Grid item>
+                <Input
+                  name="image"
+                  onChange={(event) =>
+                    event.target.files[0] !== undefined &&
+                    setFieldValue('image', event.target.files[0])
+                  }
+                  type="file"
+                />
+                <GreyButton
+                  disabled={values.image === ''}
+                  onClick={() => setFieldValue('image', '')}
+                >
+                  {t('register.discardimage')}
+                </GreyButton>
+                <FormHelperText>
+                  <ErrorMessage name="image" />
+                </FormHelperText>
+              </Grid>
+            </Grid>
+            <p className={'mb-5 text-gray-500'}>
+              {t('register.filedisclaimer', { size: 2 })}
+            </p>
+            <div className="flex justify-center">
+              <GreyButton
+                className={'mb-4 align-center'}
+                onClick={() => props.prev(values)}
               >
-                {t('register.login')}
-              </Link>
-            </span>
-          </Card>
-        </div>
-      </div>
-    </div>
+                {t('register.goback')}
+              </GreyButton>
+            </div>
+            <div className="flex justify-center">
+              <Button
+                fullWidth
+                className={clsx(classes.submitButton, 'mb-4')}
+                type="submit"
+              >
+                {t('register.submit')}
+              </Button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
