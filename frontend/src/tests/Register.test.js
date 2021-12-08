c75eb2ce7ff83ed2c1d81139c1dd4ffc6a49ad03
Gonzalo Arca <goarca@itba.edu.ar>
2021-12-08 13:08:29 -0300

    FIX: english translation fixes

--- frontend/src/tests/Register.test.js
+++ frontend/src/tests/Register.test.js
@@ -10,69 +10,74 @@ const PHONE = '123456789';
 const EMAIL = 'hijitus@itba.edu.ar';
 const PASSWORD = 'asdfghjkl';
 
-const server = setupServer(
-    ...loginHandlers
-);
-  
+const server = setupServer(...loginHandlers);
+
 setupTests(server);
 
 test('register success', async () => {
-    renderFromRoute('/register');
-    
-    await act(async () => {        
-        fireEvent.change(screen.getByTestId('username-register-input'), {
-            target: { value: USERNAME },
-        });
-        fireEvent.change(screen.getByTestId('phone-register-input'), {
-            target: { value: PHONE },
-        });
-        fireEvent.change(screen.getByTestId('email-register-input'), {
-            target: { value: EMAIL },
-        });
-        fireEvent.change(screen.getByTestId('password-register-input'), {
-            target: { value: PASSWORD },
-        });
-        fireEvent.change(screen.getByTestId('passrepeat-register-input'), {
-            target: { value: PASSWORD },
-        });
-    });
+  renderFromRoute('/register');
 
-    userEvent.click(screen.getByTestId('submit-register-input'));
-        
-    expect(await screen.findByText('Registered successfully!')).toBeInTheDocument();
+  await act(async () => {
+    fireEvent.change(screen.getByTestId('username-register-input'), {
+      target: { value: USERNAME },
+    });
+    fireEvent.change(screen.getByTestId('phone-register-input'), {
+      target: { value: PHONE },
+    });
+    fireEvent.change(screen.getByTestId('email-register-input'), {
+      target: { value: EMAIL },
+    });
+    fireEvent.change(screen.getByTestId('password-register-input'), {
+      target: { value: PASSWORD },
+    });
+    fireEvent.change(screen.getByTestId('passrepeat-register-input'), {
+      target: { value: PASSWORD },
+    });
+  });
+
+  userEvent.click(screen.getByTestId('submit-register-input'));
+
+  expect(
+    await screen.findByText('Registered successfully!')
+  ).toBeInTheDocument();
 });
 
 test('register with no fields filled', async () => {
-    renderFromRoute('/register');
-  
-    userEvent.click(screen.getByTestId('submit-register-input'));
-  
-    expect(await screen.findAllByText('This field is required')).toHaveLength(5);
+  renderFromRoute('/register');
+
+  userEvent.click(screen.getByTestId('submit-register-input'));
+
+  expect(await screen.findAllByText('This field is required')).toHaveLength(5);
 });
 
 test('register with invalid data', async () => {
-    renderFromRoute('/register');
+  renderFromRoute('/register');
 
-    await act(async () => {
-        fireEvent.change(screen.getByTestId('phone-register-input'), {
-            target: { value: 'asdfghjkl' },
-        });
-        fireEvent.change(screen.getByTestId('email-register-input'), {
-            target: { value: 'asdfghjkl' },
-        });
-        fireEvent.change(screen.getByTestId('password-register-input'), {
-            target: { value: 'aaaa' },
-        });
-        fireEvent.change(screen.getByTestId('passrepeat-register-input'), {
-            target: { value: 'bbbb' },
-        });
-    }); 
+  await act(async () => {
+    fireEvent.change(screen.getByTestId('phone-register-input'), {
+      target: { value: 'asdfghjkl' },
+    });
+    fireEvent.change(screen.getByTestId('email-register-input'), {
+      target: { value: 'asdfghjkl' },
+    });
+    fireEvent.change(screen.getByTestId('password-register-input'), {
+      target: { value: 'aaaa' },
+    });
+    fireEvent.change(screen.getByTestId('passrepeat-register-input'), {
+      target: { value: 'bbbb' },
+    });
+  });
 
-    userEvent.click(screen.getByTestId('submit-register-input'));
+  userEvent.click(screen.getByTestId('submit-register-input'));
 
-    expect(await screen.findByText('Please enter a valid phone number')).toBeInTheDocument();
-    expect(await screen.findByText('Please enter a valid email address')).toBeInTheDocument();
-    expect(await screen.findByText('Please enter at least 8 characters')).toBeInTheDocument();
-    expect(await screen.findByText("Passwords doesn't match")).toBeInTheDocument();
+  expect(
+    await screen.findByText('Please enter a valid phone number')
+  ).toBeInTheDocument();
+  expect(
+    await screen.findByText('Please enter a valid email address')
+  ).toBeInTheDocument();
+  expect(
+    await screen.findByText('Please enter at least 8 characters')
+  ).toBeInTheDocument();
+  expect(await screen.findByText("Passwords don't match")).toBeInTheDocument();
 });
-
